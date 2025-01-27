592e06d0c34f5cd36ee043ca913c7dcdb3c5bc2e
15621431123 <73216237+15621431123@users.noreply.github.com>
2025-01-27 12:06:16 +0800

    lstm-Power.py

--- lstm-Power.py
+++ lstm-Power.py
@@ -41,7 +41,7 @@ y_train, y_test = y[:split_index], y[split_index:]
 def create_model(input_shape, neuron_count, learning_rate):
     model = Sequential()
     model.add(Input(shape=input_shape))
-    model.add(LSTM(neuron_count, activation='relu'))
+    model.add(LSTM(neuron_count, activation='tanh'))
     model.add(Dense(1))
     optimizer = Adam(learning_rate=learning_rate)
     model.compile(loss='mean_squared_error', optimizer=optimizer)
